e73ebf793817b41f790acd4c7e07e0b795d84a9d
eric <maneeric20@gmail.com>
2024-04-15 13:02:00 +0300

    ppo

--- src/pages/users/Shop/shop.jsx
+++ src/pages/users/Shop/shop.jsx
@@ -36,7 +36,7 @@ export const Shop = () => {
   }, []);
 
   return (
-    <div className="shop pt-20">
+    <div className="shop pt-20" style={{ backgroundColor: '#fff' }}>
       <Nav />
       <div className="w-full mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
         {data.map((product) => (
